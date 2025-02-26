36e8462272fb31f80b73a788e9ac03d2754ed6fe
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-02-26 14:52:47 +0530

    Solution to Print a Diamond Pattern

---
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -0,0 +1,31 @@
+// Your code here...
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int row = 0; row < n; row++)
+    {
+        for(int col = 0; col < n - row - 1; col++)
+        {
+            cout<<' ';
+        }
+        for(int col = 0; col < row + 1; col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int row=0; row<n; row++)
+    {
+        for(int col=0; col<row; col++)
+        {
+            cout<<" ";
+        }
+        for(int col=0; col<n-row; col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
