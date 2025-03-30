95cf11a7b6827013a407e0aff81679b23f0d783a
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-03-30 16:01:39 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -22,26 +22,53 @@ int main() {
     // else{
     //     printf("-1");
     // }
-     int n, target;
-    //printf("Enter the elements and targets : ");
-    scanf("%d%d", &n, &target);
-    int arr[n];
-    int found = -1;
-    //printf("Enter the elements in an array: ");
-    for(int i=0; i<n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    for(int i=0; i<n; i++) {
-        if(arr[i] == target){
-            found = i;
-            break;
-        }
+    //  int n, target;
+    // //printf("Enter the elements and targets : ");
+    // scanf("%d%d", &n, &target);
+    // int arr[n];
+    // int found = -1;
+    // //printf("Enter the elements in an array: ");
+    // for(int i=0; i<n; i++) {
+    //     scanf("%d", &arr[i]);
+    // }
+    // for(int i=0; i<n; i++) {
+    //     if(arr[i] == target){
+    //         found = i;
+    //         break;
+    //     }
         
+    // }
+    // if(found != -1) {
+    //     printf("%d",found);
+    // }
+    // else{
+    //     printf("-1");
+    // }
+    int n, target;
+    //printf("Enter size and target: ");
+scanf("%d %d", &n, &target);
+
+// Input validation
+if (n <= 0) {
+    printf("-1\n");
+    return -1;
+}
+
+int arr[n];
+int found = -1;
+//printf("Enter %d elements: ", n);
+for(int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+}
+
+// Search with bounds checking
+for(int i = 0; i < n; i++) {
+    if(arr[i] == target) {
+        found = i;
+        break;
     }
-    if(found != -1) {
-        printf("%d",found);
-    }
-    else{
-        printf("-1");
-    }
+}
+
+// Output with consistent format
+printf("%d\n", found);
 }
