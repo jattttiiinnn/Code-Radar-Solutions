ffcd5e91b6b2a6cf3de05d32389f32b01da4108c
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-03-31 19:39:54 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,16 +1,55 @@
-// Your code here...
+// // Your code here...
+// #include <stdio.h>
+
+// void Movezeroes(int arr[], int n) {
+//     for(int i=0; i<n-1; i++) {
+//         for(int j=0; j<n; j++) {
+//             if(arr[j] == 0) {
+//                 for(int k=j+1; j<n; j++){
+//                     if(arr[k] != 0){
+//                         int temp = arr[j];
+//                         arr[j] = arr[k];
+//                         arr[k] = temp;
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     Movezeroes(arr, n);
+
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
-void Movezeroes(int arr[], int n) {
-    for(int i=0; i<n-1; i++) {
-        for(int j=0; j<n; j++) {
-            if(arr[j] == 0) {
-                for(int k=j+1; j<n; j++){
-                    if(arr[k] != 0){
+void moveZerosToEnd(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {  
+        for (int j = 0; j < n - i - 1; j++) {  
+            if (arr[j] == 0) {  // If current element is zero
+                // Find the next non-zero element and swap
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[k] != 0) {
+                        // Swap arr[j] and arr[k]
                         int temp = arr[j];
                         arr[j] = arr[k];
                         arr[k] = temp;
-                        break;
+                        break;  // Stop after one swap
                     }
                 }
             }
@@ -27,7 +66,7 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    Movezeroes(arr, n);
+    moveZerosToEnd(arr, n);
 
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
