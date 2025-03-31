8dfbebbffafdcfcfbcb77c960e9324a4767e6ef4
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-03-31 18:27:16 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,19 +1,19 @@
 // Your code here...
 #include <stdio.h>
 int main() {
-     int n;
-    printf("Enter the size: ");
+    int n;
+    //printf("Enter the size: ");
     scanf("%d", &n);
 
     int arr[n], copy[n];
 
-    printf("Enter the array elements: ");
+    //printf("Enter the array elements: ");
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);  // Added & operator
         copy[i] = arr[i];
     }
     int isPalindrome = 1;
-   for(int i=0; i<n; i++) {
+    for(int i=0; i<n; i++) {
         if(arr[i] != copy[i]) {
             isPalindrome = 0;
             break;
