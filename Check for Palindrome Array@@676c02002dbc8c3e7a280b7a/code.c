af8226a0fed74686a6320cec2a8d667cbf72ea1e
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-03-31 18:29:32 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,5 +1,18 @@
 // Your code here...
 #include <stdio.h>
+
+void reverse(int arr[], int n) {
+    int start = 0;
+    int end = n-1;
+
+    while(start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
 int main() {
     int n;
     //printf("Enter the size: ");
@@ -12,6 +25,9 @@ int main() {
         scanf("%d", &arr[i]);  // Added & operator
         copy[i] = arr[i];
     }
+
+    reverse(arr,n);
+
     int isPalindrome = 1;
     for(int i=0; i<n; i++) {
         if(arr[i] != copy[i]) {
