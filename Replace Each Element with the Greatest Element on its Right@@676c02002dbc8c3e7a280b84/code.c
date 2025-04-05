5aa7f8e82388330df50ca05178b85be2c72bfb4b
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-04-05 23:59:05 +0530

    Solution to Replace Each Element with the Greatest Element on its Right

--- Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
+++ Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
@@ -1,26 +1,53 @@
-// Your code here...
-#include<stdio.h>
-#include<limits.h>
+// // Your code here...
+// #include<stdio.h>
+// #include<limits.h>
+
+// int main() {
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0; i<n; i++) {
+//         scanf("%d",&arr[i]);
+//     }
+//     int new_arr[n];
+//     for(int i=0; i<n; i++) {
+//        int max = arr[i+1];
+//        for(int j=i-+1; j<n; j++){
+//         if(arr[j] > max) {
+//             max = arr[j];
+//         }
+//        }
+//        new_arr[i] = max;
+//     }
+//     new_arr[n-1] = -1;
+//     for(int i=0; i<n; i++) {
+//         printf("%d ", new_arr[i]);
+//     }
+// }
+
+#include <stdio.h>
 
 int main() {
     int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0; i<n; i++) {
-        scanf("%d",&arr[i]);
+    scanf("%d", &n);
+    int arr[n], result[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int new_arr[n];
-    for(int i=0; i<n; i++) {
-       int max = arr[i+1];
-       for(int j=i-+1; j<n; j++){
-        if(arr[j] > max) {
-            max = arr[j];
+
+    int max = -1;
+
+    for(int i = n - 1; i >= 0; i--) {
+        result[i] = max;
+        if(arr[i] > max) {
+            max = arr[i];
         }
-       }
-       new_arr[i] = max;
     }
-    new_arr[n-1] = -1;
-    for(int i=0; i<n; i++) {
-        printf("%d ", new_arr[i]);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", result[i]);
     }
+
+    return 0;
 }
