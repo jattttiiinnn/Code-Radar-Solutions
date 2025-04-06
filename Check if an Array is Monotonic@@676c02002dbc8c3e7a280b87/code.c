96a93194a1a9df89d9a8563aa6172d968b9a4718
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-04-06 11:47:08 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -1,35 +1,63 @@
-// Your code here...
-#include<stdio.h>
+// // Your code here...
+// #include<stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     int sorted[n];
+//     for(int i=0; i<n; i++) 
+//     {
+//         scanf("%d",&arr[i]);
+//         sorted[i] = arr[i];
+//     }
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=0; j<n-i-1; j++)
+//         {
+//             if(arr[j] > arr[j+1])
+//             {
+//                 int temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+//     int increasing = 1, decreasing = 1;
+//     for(int i=0; i<n; i++)
+//     {
+//         if(arr[i] > arr[i-1] ) decreasing = 0;
+//         if(arr[i] < arr[i-1]) increasing = 0;
+//     }
+//     if(increasing || decreasing) printf("YES");
+//     else printf("NO");
+// }
+
+#include <stdio.h>
 
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    int sorted[n];
-    for(int i=0; i<n; i++) 
+
+    for(int i = 0; i < n; i++) 
     {
-        scanf("%d",&arr[i]);
-        sorted[i] = arr[i];
+        scanf("%d", &arr[i]);
     }
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n-i-1; j++)
-        {
-            if(arr[j] > arr[j+1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+
     int increasing = 1, decreasing = 1;
-    for(int i=0; i<n; i++)
+    for(int i = 1; i < n; i++)
     {
-        if(arr[i] > arr[i-1] ) decreasing = 0;
-        if(arr[i] < arr[i-1]) increasing = 0;
+        if(arr[i] > arr[i - 1]) decreasing = 0;
+        if(arr[i] < arr[i - 1]) increasing = 0;
     }
-    if(increasing || decreasing) printf("YES");
-    else printf("NO");
+
+    if(increasing || decreasing)
+        printf("YES\n");  // Monotonic
+    else
+        printf("NO\n");   // Not monotonic
+
+    return 0;
 }
