1f93d5056e2c7e43fe19f3de1318974bc3b9ba8f
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-04-11 13:09:42 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -1,27 +1,63 @@
+// #include <stdio.h>
+// #include <limits.h>
+// int main()
+// {
+//      int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for(int i=0; i<n; i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     int min = INT_MIN;
+//     int leaders[n];
+//     int index = 0;
+//     for(int i=0; i<n; i++)
+//     {
+//         if(arr[i] > min)
+//         {
+//             leaders[index++] = arr[i];
+//             min = arr[i];
+//         }
+//     }
+//     for(int i=index - 1; i>=0; i--)
+//     {
+//         printf("%d ", leaders[i]);
+//     }
+// }
+
 #include <stdio.h>
 #include <limits.h>
+
 int main()
 {
-     int n;
-    scanf("%d",&n);
+    int n;
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
-    int min = INT_MIN;
-    int leaders[n];
+
+    int maxRight = INT_MIN;
+    int leaders[n];  // to store leaders temporarily
     int index = 0;
-    for(int i=0; i<n; i++)
+
+    // Traverse from right to left
+    for (int i = n - 1; i >= 0; i--)
     {
-        if(arr[i] > min)
+        if (arr[i] > maxRight)
         {
             leaders[index++] = arr[i];
-            min = arr[i];
+            maxRight = arr[i];
         }
     }
-    for(int i=index - 1; i>=0; i--)
+
+    // Print in reverse to restore order
+    for (int i = index - 1; i >= 0; i--)
     {
         printf("%d ", leaders[i]);
     }
+
+    return 0;
 }
