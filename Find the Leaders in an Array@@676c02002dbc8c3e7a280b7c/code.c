cab673b6709b8086e9ee567b7a1eb0c0d5fdbc05
Jatin Kumar <106923126+jattttiiinnn@users.noreply.github.com>
2025-04-11 13:03:38 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -12,7 +12,7 @@ int main()
     int min = INT_MIN;
     for(int i=0; i<n; i++)
     {
-        if(arr[i] > min || arr[i] > arr[i+1])
+        if(arr[i] > min || arr[i] > arr[i+1] || arr[i] != arr[i+1])
         {
             printf("%d ",arr[i]);
         }
